c6500a782ff6da0871469344b87cfe88da5d0f1d
David <david.davidyao.yao@gmail.com>
2024-07-20 12:24:27 -0500

    vec3 helper class

--- RayTracer/src/Main.cpp
+++ RayTracer/src/Main.cpp
@@ -16,6 +16,7 @@ int main() {
     MyFile << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; j++) {
+        cout << "\rScanlines remaining: " << image_height - j << ' ' << flush;
         for (int i = 0; i < image_width; i++) {
             auto r = double(i) / (image_width - 1);
             auto g = double(j) / (image_height - 1);
