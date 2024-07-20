4dce151de7b9bf7a721d5ad7dc1a42e4b51ca51b
David <david.davidyao.yao@gmail.com>
2024-07-20 11:36:12 -0500

    writes test image to PPM file

--- RayTracer/src/Main.cpp
+++ RayTracer/src/Main.cpp
@@ -1,8 +1,35 @@
-// Your First C++ Program
-
 #include <iostream>
+#include <fstream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello";
-    return 0;
+
+    // Image
+
+    int image_width = 256;
+    int image_height = 256;
+
+    // Render
+
+    ofstream MyFile("filename.ppm");
+
+    MyFile << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; j++) {
+        for (int i = 0; i < image_width; i++) {
+            auto r = double(i) / (image_width - 1);
+            auto g = double(j) / (image_height - 1);
+            auto b = 0.0;
+
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
+
+            MyFile << ir << ' ' << ig << ' ' << ib << '\n';
+
+            
+        }
+    }
+
+    MyFile.close();
 }
