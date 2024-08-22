74c47d41eb73283dc291f341ab4c8195e3d3bf5c
David <david.davidyao.yao@gmail.com>
2024-08-22 17:48:14 -0500

    Added ray tracing for simple sphere

--- RayTracer/src/Main.cpp
+++ RayTracer/src/Main.cpp
@@ -1,13 +1,55 @@
 #include <iostream>
 #include <fstream>
+
+#include "../include/vec3.h"
+#include "../include/color.h"
+#include "../include/ray.h"
+
 using namespace std;
+
+bool hit_sphere(const ray& r) {
+
+    vec3 sphere_center = vec3(0, 0, -1);
+    double sphere_radius = 0.5;
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), sphere_center - r.at(0));
+    auto c = dot(sphere_center - r.at(0), sphere_center - r.at(0)) - sphere_radius * sphere_radius;
+
+    auto discriminant = b * b - 4 * a * c;
+
+    return (discriminant > 0);
+}
+
+color ray_color(const ray& r) {
+    vec3 unit_ray = unit_vector(r.direction());
+    auto t = 0.5 * (unit_ray.y() + 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
+ray get_pixel_ray(const int x, const int y, const int image_width, const int image_height) {
+
+    auto viewport_height = 2.0;
+    auto viewport_width = (static_cast<double>(image_width) / image_height) * viewport_height;
+
+    auto pixel_x_size = viewport_width / image_width;
+    auto pixel_y_size = viewport_height / image_height;
+
+    vec3 top_left_pixel = vec3(-viewport_width / 2 + pixel_x_size, viewport_height / 2 + pixel_y_size, -1);
+
+    vec3 pixel = vec3(top_left_pixel.x() + x * pixel_x_size, top_left_pixel.y() - y * pixel_y_size, top_left_pixel.z());
+
+    return ray(vec3(0, 0, 0), pixel);
+
+}
 
 int main() {
 
-    // Image
+    auto aspect_ratio = 16.0 / 9.0;
 
     int image_width = 256;
-    int image_height = 256;
+    int image_height = static_cast<int>(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;    // prevent image_height from being 0
+
 
     // Render
 
@@ -18,17 +60,22 @@ int main() {
     for (int j = 0; j < image_height; j++) {
         cout << "\rScanlines remaining: " << image_height - j << ' ' << flush;
         for (int i = 0; i < image_width; i++) {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.0;
-
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
-
-            MyFile << ir << ' ' << ig << ' ' << ib << '\n';
-
             
+            ray r = get_pixel_ray(i, j, image_width, image_height);
+
+            auto pixel_color = ray_color(r);
+
+            bool hit = hit_sphere(r);
+
+            if (hit) {
+				write_color(MyFile, color(255, 0, 0));
+			} else {
+				write_color(MyFile, pixel_color);
+			}
+
+//            write_color(MyFile, pixel_color);
+
+
         }
     }
 
